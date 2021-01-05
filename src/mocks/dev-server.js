ecae1e5f0685cbf4d0425ba44911077e4d2521f7
marcosvega91 <marcomoretti0103@gmail.com>
2021-01-05 12:42:44 +0100

    feat: use msw-data

--- src/mocks/dev-server.js
+++ src/mocks/dev-server.js
@@ -1,8 +1,21 @@
 import {setupWorker} from 'msw'
-import {handlers} from './handlers'
+import {factory, primaryKey, identity} from '@mswjs/data'
+import {random} from 'faker'
 
 
-const worker = setupWorker(...handlers)
+const db = factory({
+  todos:{
+    id:primaryKey(random.uuid),
+    title: String,
+    completed: identity(false)
+  }
+})
+
+db.todos.create({
+  title: 'go to work'
+})
+
+const worker = setupWorker(...db.todos.toHandlers(process.env.REACT_APP_API))
 
 
 export {worker}
