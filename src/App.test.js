30c11b0bf0d46e1c5fabc2b3b7ada113513f6c46
marcosvega91 <marcomoretti0103@gmail.com>
2021-01-05 12:23:50 +0100

    test: test add new todo and mark as completed

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, within } from '@testing-library/react';
+import userEvent,{specialChars} from '@testing-library/user-event'
 import App from './App';
 
-test('renders learn react link', () => {
+test('add a new todo', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const todoInput = screen.getByRole('textbox', { name: /add input/i });
+  userEvent.type(todoInput, `go to market${specialChars.enter}`)
+
+  const todoListQuery = within(screen.getByRole('list'))
+  await waitFor(() => {
+    expect(todoListQuery.getByText('go to market')).toBeInTheDocument()
+  })
+});
+
+test('mark todo as completed', async () => {
+  render(<App />);
+  const todoInput = screen.getByRole('textbox', { name: /add input/i });
+  userEvent.type(todoInput, `go to market${specialChars.enter}`)
+
+  
+  const todoListQuery = within(screen.getByRole('list'))
+  await waitFor(() => {
+    expect(todoListQuery.getByText('go to market')).toBeInTheDocument()
+    
+  })
+
+  const todo = todoListQuery.getByText('go to market')
+  expect(todo).not.toHaveClass('completed')
+  
+  userEvent.click(todo)
+  
+  await waitFor(() => {
+    expect(todo).toHaveClass('completed')
+  })
+
 });
