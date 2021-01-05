a5975765f1a82707931a9d9d73ccfd4c5f1a7dc3
marcosvega91 <marcomoretti0103@gmail.com>
2021-01-05 10:49:16 +0100

    fix: set content type and get todos on bootstrap

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ const ENTER_KEY = 13
 function App() {
   const [todos, setTodos] = React.useState([])
 
+  React.useEffect(() => {
+    fetch(new URL('todos', process.env.REACT_APP_API)).then(async (response) => {
+      const body = await response.json()
+      setTodos(body)
+    })
+  },[])
+
   const handleNewTodoKeyDown = async (event) => {
     if (event.keyCode !== ENTER_KEY) {
       return;
@@ -16,27 +23,33 @@ function App() {
     
     const response = await fetch(new URL('todos', process.env.REACT_APP_API),{
       method:'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
       body:JSON.stringify({
         title:event.target.value
       })
     })
-
+    event.target.value = ""
     const todo = await response.json()
     setTodos((todos) => [...todos,todo])
   }
 
   const markComplete = async (todo) => {
-    await fetch(new URL(`todos/${todo.id}`, process.env.REACT_APP_API),{
+    const response = await fetch(new URL(`todos/${todo.id}`, process.env.REACT_APP_API),{
       method:'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
       body:JSON.stringify({
         completed:!todo.completed
       })
     })
+    const updatedTodo = await response.json()
 
     setTodos((todos) => {
-      todo.completed = !todo.completed
       const indexTodo = todos.findIndex((currentTodo) => currentTodo.id === todo.id)
-      return [...todos.slice(0, indexTodo), todo, ...todos.slice(indexTodo+1)]
+      return [...todos.slice(0, indexTodo), updatedTodo, ...todos.slice(indexTodo+1)]
     } )
   }
 
